f146e2b2b3700e1f2d2bba52460992d19b7141ac
Hany Afifi <hanyafifi@gmail.com>
2020-09-29 18:58:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var fibonacci = function (n)
+{
+  if (n===1)
+  {
+    return [0, 1];
+  }
+  else
+  {
+    var s = fibonacci (n - 1);
+    s.push(s[s.length - 1] + s[s.length - 2]);
+    return s;
+  }
+};
+
+ console.log(fibonacci_series(8));
